70e897a37fc1d341227c91b744003a4872a294fc
Piyush Goyani <piyush.goyani@ezeetechnosys.com>
2021-05-14 11:08:40 +0530

    setup vue3 project
     - add SLDS and style

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import "@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css";
 createApp(App).use(router).mount('#app')
